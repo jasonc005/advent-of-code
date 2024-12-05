74fda637b7b435d4da9a9762baaaa603d2f44308
Jason Chandler <jason.chandlerp@gmail.com>
2024-12-04 21:40:52 -0500

    Reformat solutions into single project

---
+++ AoC_2024/Program.cs
@@ -0,0 +1,23 @@
+﻿using AoC_2024.Solutions;
+
+var exit = false;
+do
+{
+    Console.Write("\nSelect Day: ");
+    var day = Console.ReadLine();
+
+    switch (day)
+    {
+        case "1":
+            Day01.Run(); break;
+        case "2":
+            Day02.Run(); break;
+        case "3":
+            Day03.Run(); break;
+        case "4":
+            Day04.Run(); break;
+        default:
+            exit = true; break;
+    }
+
+} while (!exit);
---
+++ AoC_2024/Solutions/Day01.cs
@@ -0,0 +1,47 @@
+﻿namespace AoC_2024.Solutions
+{
+    public static class Day01
+    {
+        public static void Run()
+        {
+            // Create lists
+            var leftList = new List<int>();
+            var rightList = new List<int>();
+
+            using (var file = new StreamReader("Input/day01_input.txt"))
+            {
+                string? record;
+
+                while ((record = file.ReadLine()) != null)
+                {
+                    var items = record.Split("   ").Select(x => int.Parse(x)).ToList();
+                    leftList.Add(items[0]);
+                    rightList.Add(items[1]);
+                }
+            }
+
+            // Part One
+            leftList.Sort();
+            rightList.Sort();
+
+            var totalDistance = 0;
+
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                totalDistance += Math.Abs(leftList[i] - rightList[i]);
+            }
+
+            Console.WriteLine($"Total Distance: {totalDistance}");
+
+            // Part Two
+            var similarityScore = 0;
+
+            foreach (var leftItem in leftList)
+            {
+                similarityScore += leftItem * rightList.Count(rightItem => leftItem == rightItem);
+            }
+
+            Console.WriteLine($"Similarity Score: {similarityScore}");
+        }
+    }
+}
